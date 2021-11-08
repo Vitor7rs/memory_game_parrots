993305ce044b86d39bc4d31c2217e14d8c43bc48
Vitor <vitorjvbm@gmail.com>
2021-11-08 14:05:21 -0300

    cliques na carta e contagem de jogadas para a vitoria

--- scripts.js
+++ scripts.js
@@ -7,13 +7,13 @@ while ((totalCartas%2) !== 0 || (totalCartas > 14) || (totalCartas < 4)) {
 
 //lista de imagens
 let imagem = [
-    "<img class ='desaparecer' src='img/bobrossparrot.gif' alt=''>",
-    "<img class ='desaparecer' src='img/explodyparrot.gif' alt=''>",
-    "<img class ='desaparecer' src='img/fiestaparrot.gif' alt=''>",
-    "<img class ='desaparecer' src='img/metalparrot.gif' alt=''>",
-    "<img class ='desaparecer' src='img/tripletsparrot.gif' alt=''>",
-    "<img class ='desaparecer' src='img/unicornparrot.gif' alt=''>",
-    "<img class ='desaparecer' src='img/revertitparrot.gif' alt=''>"
+    "<img  src='img/bobrossparrot.gif' alt=''>",
+    "<img  src='img/explodyparrot.gif' alt=''>",
+    "<img  src='img/fiestaparrot.gif' alt=''>",
+    "<img  src='img/metalparrot.gif' alt=''>",
+    "<img  src='img/tripletsparrot.gif' alt=''>",
+    "<img  src='img/unicornparrot.gif' alt=''>",
+    "<img  src='img/revertitparrot.gif' alt=''>"
 ]
 
 //criando cards dentro de uma lista
@@ -22,27 +22,27 @@ let cardsLista=[];
 for(let i = 0; i <=((totalCartas/2)-1); i++ ){
     
     cardsLista.push(`
-            <li class='card'>
-                <div class='frente-card'>
-                    <img src='img/front.png' alt=''>
-                </div>
-                
-                <div class='fundo-card'>
-                    ${imagem[i]}
-                </div>
-            </li>`)
+        <li class='card' onclick="turno(this)">
+            <div class='frente-card'>
+                <img src='img/front.png' alt=''>
+            </div>      
+            <div class='img desaparecer'>
+                ${imagem[i]}
+            </div>
+        </li>`)
            
-            cardsLista.push(`
-            <li class='card'>
-                <div class='frente-card'>
-                    <img src='img/front.png' alt=''>
-                </div>
+    cardsLista.push(`
+        <li class='card' onclick="turno(this)">
+            <div class='frente-card'>
+                <img src='img/front.png' alt=''>
+            </div>
                 
-                <div class='fundo-card'>
-                    ${imagem[i]}
-                </div>
-            </li>`)
+            <div class='img desaparecer'>
+                ${imagem[i]}
+            </div>
+        </li>`)
 }
+
 //embaralhando lista
 function embaralhador() { 
 	return Math.random() - 0.5; 
@@ -55,5 +55,54 @@ for (let i = 0; i < cardsLista.length; i++) {
     cards.innerHTML += `${cardsLista[i]}`;
 }
 
+// VERIFICANDO AS JOGADAS E SALVANDO CARTAS SELECIONADAS
+ let jogadas = 0;
+ let cartaVirada = [];
+ let fimDeJogo=0;
 
+ function turno(carta){
+    jogadas+=1;
+    const frente = carta.children[0];
+    frente.classList.add('desaparecer');
 
+    const atras = carta.children[1];
+    atras.classList.remove('desaparecer');
+
+    cartaVirada.push(atras);
+
+    if(cartaVirada.length===2){
+
+        verificar(cartaVirada[0], cartaVirada[1]);
+        cartaVirada=[]
+    }
+    
+ }
+
+ // verificar se as cartas são iguais e chamar a funçao que vira ela, caso nesessário
+function verificar(carta1, carta2){
+    let frente1 = carta1.innerHTML;
+    let frente2= carta2.innerHTML;
+    
+    if(frente1 === frente2){
+        carta1.removeAttribute('onclick');
+        console.log(carta1, carta2);
+        fimDeJogo+=2;
+        setTimeout(vitoria, 1000);
+    }
+    else setTimeout(virar, 1000, carta1, carta2);
+}
+
+// vira as cartas, caso nao tenham dado match
+function virar(carta1, carta2){
+    carta1.parentNode.children[0].classList.remove('desaparecer');
+    carta1.parentNode.children[1].classList.add('desaparecer');
+    carta2.parentNode.children[0].classList.remove('desaparecer');
+    carta2.parentNode.children[1].classList.add('desaparecer');
+}
+
+// verifica o fim de jogo
+function vitoria(){
+    if(fimDeJogo === totalCartas){
+        alert("Você venceu em " + jogadas + " jogadas");
+    }
+}
